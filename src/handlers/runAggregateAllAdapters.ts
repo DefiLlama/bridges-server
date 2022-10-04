e7015fdef714840c8b7df4f3f7821f64b27624a4
Cocoahomology <83151536+Cocoahomology@users.noreply.github.com>
2022-10-04 15:02:25 -0400

    add aggregation job

---
+++ src/handlers/runAggregateAllAdapters.ts
@@ -0,0 +1,13 @@
+import { wrapScheduledLambda } from "../utils/wrap";
+import { convertToUnixTimestamp } from "../utils/date";
+import { runAggregateDataAllAdapters } from "../utils/aggregate";
+
+export default wrapScheduledLambda(async (_event) => {
+  const currentDate = new Date();
+  const currentTimestamp = convertToUnixTimestamp(currentDate);
+  await runAggregateDataAllAdapters(currentTimestamp, true);
+  const currentHour = currentDate.getHours();
+  if (currentHour === 0) {
+    await runAggregateDataAllAdapters(currentTimestamp, false);
+  }
+});
