2bcf9e521708b13acfc1477a86a8d9c620317089
Cocoahomology <83151536+Cocoahomology@users.noreply.github.com>
2022-10-04 21:34:51 -0400

    missing awaits

--- src/handlers/checkDbEntries.ts
+++ src/handlers/checkDbEntries.ts
@@ -12,36 +12,47 @@ export default wrapScheduledLambda(async (_event) => {
   const timestampAtStartOfDay = getTimestampAtStartOfDay(getCurrentUnixTimestamp());
   const startTimestamp = timestampAtStartOfDay - secondsInDay + 1;
   const endTimestamp = timestampAtStartOfDay;
-  bridgeNetworks.map(async (bridgeNetwork) => {
-    const { bridgeDbName } = bridgeNetwork;
-    const adapter = adapters[bridgeDbName];
-    if (!adapter) {
-      throw new Error(`Adapter for ${bridgeDbName} not found, check it is exported correctly.`);
-    }
-    Object.keys(adapter).map(async (chain) => {
-      const hourlyEntries = await queryAggregatedHourlyTimestampRange(
-        startTimestamp,
-        endTimestamp,
-        bridgeDbName,
-        chain
+  await Promise.all(
+    bridgeNetworks.map(async (bridgeNetwork) => {
+      const { bridgeDbName } = bridgeNetwork;
+      const adapter = adapters[bridgeDbName];
+      if (!adapter) {
+        throw new Error(`Adapter for ${bridgeDbName} not found, check it is exported correctly.`);
+      }
+      await Promise.all(
+        Object.keys(adapter).map(async (chain) => {
+          const hourlyEntries = await queryAggregatedHourlyTimestampRange(
+            startTimestamp,
+            endTimestamp,
+            bridgeDbName,
+            chain
+          );
+          const dailyEntries = await queryAggregatedDailyTimestampRange(
+            startTimestamp,
+            endTimestamp,
+            bridgeDbName,
+            chain
+          );
+          console.info(bridgeDbName, hourlyEntries.length);
+          console.info(bridgeDbName, dailyEntries.length);
+          if (hourlyEntries.length < 24) {
+            await insertErrorRow({
+              ts: getCurrentUnixTimestamp() * 1000,
+              target_table: "hourly_aggregated",
+              keyword: "data",
+              error: `Bridge ${bridgeDbName} on chain ${chain} has ${hourlyEntries.length} hourly entries from ${startTimestamp} to ${endTimestamp}`,
+            });
+          }
+          if (!dailyEntries.length) {
+            await insertErrorRow({
+              ts: getCurrentUnixTimestamp() * 1000,
+              target_table: "daily_aggregated",
+              keyword: "data",
+              error: `Bridge ${bridgeDbName} on chain ${chain} has NO daily entry for ${endTimestamp}.`,
+            });
+          }
+        })
       );
-      const dailyEntries = await queryAggregatedDailyTimestampRange(startTimestamp, endTimestamp, bridgeDbName, chain);
-      if (hourlyEntries.length < 24) {
-        await insertErrorRow({
-          ts: getCurrentUnixTimestamp() * 1000,
-          target_table: "hourly_aggregated",
-          keyword: "data",
-          error: `Bridge ${bridgeDbName} on chain ${chain} has ${hourlyEntries.length} hourly entries from ${startTimestamp} to ${endTimestamp}`,
-        });
-      }
-      if (!dailyEntries.length) {
-        await insertErrorRow({
-          ts: getCurrentUnixTimestamp() * 1000,
-          target_table: "daily_aggregated",
-          keyword: "data",
-          error: `Bridge ${bridgeDbName} on chain ${chain} has NO daily entry for ${endTimestamp}.`,
-        });
-      }
-    });
-  });
+    })
+  );
 });
