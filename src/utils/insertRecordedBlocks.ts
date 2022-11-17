9a1cda92879a67638dd91739ac5b3926712d8a86
Cocoahomology <83151536+Cocoahomology@users.noreply.github.com>
2022-11-16 19:17:54 -0500

    fix insertRecordedBlocks chain mapping

--- src/utils/insertRecordedBlocks.ts
+++ src/utils/insertRecordedBlocks.ts
@@ -2,6 +2,8 @@ import recordedBlocksRecord from "./recordedBlocks.json";
 import adapters from "../adapters";
 import { lookupBlock } from "@defillama/sdk/build/util";
 import { Chain } from "@defillama/sdk/build/general";
+import bridgeNetworks from "../data/bridgeNetworkData";
+import { importBridgeNetwork } from "../data/importBridgeNetwork";
 const FileSystem = require("fs");
 
 const insertRecordedBlocks = async (adapterName: string, startTimestamp: number, endTimestamp: number) => {
@@ -10,13 +12,17 @@ const insertRecordedBlocks = async (adapterName: string, startTimestamp: number,
   if (!adapter) {
     throw new Error(`Adapter for ${adapterName} not found, check it is exported correctly.`);
   }
+  const bridgeNetwork = importBridgeNetwork(adapterName)!;
   const blockPromises = Promise.all(
     Object.keys(adapter).map(async (chain) => {
+      const chainContractsAreOn = bridgeNetwork.chainMapping?.[chain as Chain]
+      ? bridgeNetwork.chainMapping?.[chain as Chain]
+      : chain;
       if (recordedBlocks[`${adapterName}:${chain}`]) {
         console.info(`Adapter ${adapterName} has recorded blocks entry on chain ${chain}, skipping.`);
       } else {
-        const startBlock = (await lookupBlock(startTimestamp, { chain: chain as Chain })).block;
-        const endBlock = (await lookupBlock(endTimestamp, { chain: chain as Chain })).block;
+        const startBlock = (await lookupBlock(startTimestamp, { chain: chainContractsAreOn as Chain })).block;
+        const endBlock = (await lookupBlock(endTimestamp, { chain: chainContractsAreOn as Chain })).block;
         if (!(startBlock && endBlock)) {
           throw new Error(`Could not get blocks for chain ${chain}.`);
         }
@@ -33,4 +39,4 @@ const insertRecordedBlocks = async (adapterName: string, startTimestamp: number,
   });
 };
 
-insertRecordedBlocks("chainport", 1661904000, 1667458800);
+insertRecordedBlocks("polynetwork", 1661904000, 1668618000);
