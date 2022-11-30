498ecb3315eac319c14c6175742de657b8e952e7
Cocoahomology <83151536+Cocoahomology@users.noreply.github.com>
2022-11-30 09:17:12 -0500

    add getBridge handler, update getBridges

---
+++ src/handlers/getBridge.ts
@@ -0,0 +1,157 @@
+import { IResponse, successResponse, errorResponse } from "../utils/lambda-response";
+import wrap from "../utils/wrap";
+import { getDailyBridgeVolume, getHourlyBridgeVolume } from "../utils/bridgeVolume";
+import { secondsInDay, getCurrentUnixTimestamp, secondsInHour } from "../utils/date";
+import getAggregatedDataClosestToTimestamp from "../utils/getRecordClosestToTimestamp";
+import { importBridgeNetwork } from "../data/importBridgeNetwork";
+
+const getBridge = async (bridgeNetworkId?: number) => {
+  const bridgeNetwork = importBridgeNetwork(undefined, bridgeNetworkId);
+
+  if (!bridgeNetwork) {
+    return errorResponse({
+      message: "Bridge ID not found.",
+    });
+  }
+  const { id, bridgeDbName, url, displayName, iconLink, chains, destinationChain } = bridgeNetwork;
+  let chainBreakdown = {} as any;
+  await Promise.all(
+    [...chains, "all"].map(async (chain) => {
+      const queryChain = chain === "all" ? undefined : chain.toLowerCase();
+
+      let lastHourlyVolume, lastDailyVolume, dayBeforeLastVolume;
+      let currentDayVolume = 0;
+      let weeklyVolume = 0;
+      let monthlyVolume = 0;
+      let dayBeforeLastTxs = {} as any;
+      let prevDayTxs = {} as any;
+      let lastHourlyTxs = {} as any;
+      let currentDayTxs = {} as any;
+      let weeklyTxs = {} as any;
+      let monthlyTxs = {} as any;
+      const currentTimestamp = getCurrentUnixTimestamp();
+      const dailyStartTimestamp = currentTimestamp - 30 * secondsInDay;
+      const lastMonthDailyVolume = await getDailyBridgeVolume(dailyStartTimestamp, currentTimestamp, queryChain, id);
+      let lastDailyTs = 0;
+      if (lastMonthDailyVolume?.length) {
+        const lastDailyVolumeRecord = lastMonthDailyVolume[lastMonthDailyVolume.length - 1];
+        lastDailyTs = parseInt(lastDailyVolumeRecord.date);
+        lastDailyVolume = (lastDailyVolumeRecord.depositUSD + lastDailyVolumeRecord.withdrawUSD) / 2;
+        prevDayTxs = { deposits: lastDailyVolumeRecord.depositTxs, withdrawals: lastDailyVolumeRecord.withdrawTxs };
+
+        const dayBeforeLastVolumeRecord = await getAggregatedDataClosestToTimestamp(
+          lastDailyTs - secondsInDay,
+          secondsInDay,
+          false,
+          queryChain,
+          id
+        );
+        if (dayBeforeLastVolumeRecord && Object.keys(dayBeforeLastVolumeRecord).length > 0) {
+          dayBeforeLastVolume = (dayBeforeLastVolumeRecord.depositUSD + dayBeforeLastVolumeRecord?.withdrawUSD) / 2;
+          dayBeforeLastTxs = {
+            deposits: dayBeforeLastVolumeRecord.depositTxs,
+            withdrawals: dayBeforeLastVolumeRecord.withdrawTxs,
+          };
+        }
+        lastMonthDailyVolume.map((entry, i) => {
+          const volume = (entry.depositUSD + entry.withdrawUSD) / 2;
+          const { depositTxs, withdrawTxs } = entry;
+          monthlyVolume += volume;
+          monthlyTxs.deposits = (monthlyTxs.deposits ?? 0) + depositTxs;
+          monthlyTxs.withdrawals = (monthlyTxs.withdrawals ?? 0) + withdrawTxs;
+          if (i < 7) {
+            weeklyVolume += volume;
+            weeklyTxs.deposits = (weeklyTxs.deposits ?? 0) + depositTxs;
+            weeklyTxs.withdrawals = (weeklyTxs.withdrawals ?? 0) + withdrawTxs;
+          }
+        });
+      }
+
+      const hourlyStartTimestamp = currentTimestamp - secondsInDay;
+      const lastDayHourlyVolume = await getHourlyBridgeVolume(hourlyStartTimestamp, currentTimestamp, queryChain, id);
+      if (lastDayHourlyVolume?.length) {
+        const lastHourlyVolumeRecord = lastDayHourlyVolume[lastDayHourlyVolume.length - 1];
+        const { date } = lastHourlyVolumeRecord;
+        if (Math.abs(currentTimestamp - date) < 2 * secondsInHour) {
+          lastHourlyVolume = (lastHourlyVolumeRecord.depositUSD + lastHourlyVolumeRecord.withdrawUSD) / 2;
+          lastHourlyTxs = {
+            deposits: lastHourlyVolumeRecord.depositTxs,
+            withdrawals: lastHourlyVolumeRecord.withdrawTxs,
+          };
+        }
+
+        lastDayHourlyVolume.map((entry) => {
+          const volume = (entry.depositUSD + entry.withdrawUSD) / 2;
+          const { date, depositTxs, withdrawTxs } = entry;
+          // lastDailyTs is timestamp at 00:00 UTC of *previous* day
+          if (parseInt(date) > lastDailyTs + secondsInDay) {
+            currentDayVolume += volume;
+            currentDayTxs.deposits = (currentDayTxs.deposits ?? 0) + depositTxs;
+            currentDayTxs.withdrawals = (currentDayTxs.withdrawals ?? 0) + withdrawTxs;
+          }
+        });
+      }
+      chainBreakdown[chain] = {
+        lastHourlyVolume: lastHourlyVolume ?? 0,
+        currentDayVolume: currentDayVolume ?? 0,
+        lastDailyVolume: lastDailyVolume ?? 0,
+        dayBeforeLastVolume: dayBeforeLastVolume ?? 0,
+        weeklyVolume: weeklyVolume ?? 0,
+        monthlyVolume: monthlyVolume ?? 0,
+        lastHourlyTxs: Object.keys(lastHourlyTxs).length ? lastHourlyTxs : { deposits: 0, withdrawals: 0 },
+        currentDayTxs: Object.keys(currentDayTxs).length ? currentDayTxs : { deposits: 0, withdrawals: 0 },
+        prevDayTxs: Object.keys(prevDayTxs).length ? prevDayTxs : { deposits: 0, withdrawals: 0 },
+        dayBeforeLastTxs: Object.keys(dayBeforeLastTxs).length ? dayBeforeLastTxs : { deposits: 0, withdrawals: 0 },
+        weeklyTxs: Object.keys(weeklyTxs).length ? weeklyTxs : { deposits: 0, withdrawals: 0 },
+        monthlyTxs: Object.keys(monthlyTxs).length ? monthlyTxs : { deposits: 0, withdrawals: 0 },
+      };
+    })
+  );
+
+  const {
+    lastHourlyVolume,
+    currentDayVolume,
+    lastDailyVolume,
+    dayBeforeLastVolume,
+    weeklyVolume,
+    monthlyVolume,
+    lastHourlyTxs,
+    currentDayTxs,
+    prevDayTxs,
+    dayBeforeLastTxs,
+    weeklyTxs,
+    monthlyTxs,
+  } = chainBreakdown["all"];
+  delete chainBreakdown["all"];
+
+  const response = {
+    id: id,
+    name: bridgeDbName,
+    displayName: displayName,
+    // url: url,
+    lastHourlyVolume: lastHourlyVolume ?? 0,
+    currentDayVolume: currentDayVolume ?? 0,
+    lastDailyVolume: lastDailyVolume ?? 0,
+    dayBeforeLastVolume: dayBeforeLastVolume ?? 0,
+    weeklyVolume: weeklyVolume ?? 0,
+    monthlyVolume: monthlyVolume ?? 0,
+    lastHourlyTxs: Object.keys(lastHourlyTxs).length ? lastHourlyTxs : { deposits: 0, withdrawals: 0 },
+    currentDayTxs: Object.keys(currentDayTxs).length ? currentDayTxs : { deposits: 0, withdrawals: 0 },
+    prevDayTxs: Object.keys(prevDayTxs).length ? prevDayTxs : { deposits: 0, withdrawals: 0 },
+    dayBeforeLastTxs: Object.keys(dayBeforeLastTxs).length ? dayBeforeLastTxs : { deposits: 0, withdrawals: 0 },
+    weeklyTxs: Object.keys(weeklyTxs).length ? weeklyTxs : { deposits: 0, withdrawals: 0 },
+    monthlyTxs: Object.keys(monthlyTxs).length ? monthlyTxs : { deposits: 0, withdrawals: 0 },
+    chainBreakdown,
+    destinationChain: destinationChain ?? "false",
+  } as any;
+
+  return response;
+};
+
+const handler = async (event: AWSLambda.APIGatewayEvent): Promise<IResponse> => {
+  const bridgeNetworkId = parseInt(event.pathParameters?.id ?? "0");
+  const response = await getBridge(bridgeNetworkId);
+  return successResponse(response, 10 * 60); // 10 mins cache
+};
+
+export default wrap(handler);
--- src/handlers/getBridges.ts
+++ src/handlers/getBridges.ts
@@ -1,8 +1,8 @@
 import { IResponse, successResponse } from "../utils/lambda-response";
 import wrap from "../utils/wrap";
-import { getDailyBridgeVolume } from "../utils/bridgeVolume";
+import { getDailyBridgeVolume, getHourlyBridgeVolume } from "../utils/bridgeVolume";
 import { craftBridgeChainsResponse } from "./getBridgeChains";
-import { secondsInDay, secondsInWeek, getCurrentUnixTimestamp } from "../utils/date";
+import { secondsInDay, getCurrentUnixTimestamp } from "../utils/date";
 import getAggregatedDataClosestToTimestamp from "../utils/getRecordClosestToTimestamp";
 import bridgeNetworks from "../data/bridgeNetworkData";
 
@@ -11,84 +11,69 @@ const getBridges = async () => {
     await Promise.all(
       bridgeNetworks.map(async (bridgeNetwork) => {
         const { id, bridgeDbName, url, displayName, iconLink, chains, destinationChain } = bridgeNetwork;
-        // can use chains to give current chain breakdown, but not needed at this time
+        // can use chains to give chain breakdown, but not needed at this time (put in getBridge to reduce queries?)
 
+        let lastHourlyVolume, lastDailyVolume, dayBeforeLastVolume;
+        let currentDayVolume = 0;
+        let weeklyVolume = 0;
+        let monthlyVolume = 0;
         const currentTimestamp = getCurrentUnixTimestamp();
-        const startTimestamp = currentTimestamp - 7 * secondsInDay;
-        const lastWeekDailyVolume = await getDailyBridgeVolume(startTimestamp, currentTimestamp, undefined, id);
-
-        let lastDailyVolume, dayBeforeLastVolume, lastWeeklyVolume, lastMonthlyVolume;
-        if (lastWeekDailyVolume?.length) {
-          const lastDailyVolumeRecord = lastWeekDailyVolume[lastWeekDailyVolume.length - 1];
-          const lastTS = parseInt(lastDailyVolumeRecord.date);
+        const dailyStartTimestamp = currentTimestamp - 30 * secondsInDay;
+        const lastMonthDailyVolume = await getDailyBridgeVolume(dailyStartTimestamp, currentTimestamp, undefined, id);
+        let lastDailyTs = 0;
+        if (lastMonthDailyVolume?.length) {
+          const lastDailyVolumeRecord = lastMonthDailyVolume[lastMonthDailyVolume.length - 1];
+          lastDailyTs = parseInt(lastDailyVolumeRecord.date);
           lastDailyVolume = (lastDailyVolumeRecord.depositUSD + lastDailyVolumeRecord.withdrawUSD) / 2;
 
           const dayBeforeLastVolumeRecord = await getAggregatedDataClosestToTimestamp(
-            lastTS - secondsInDay,
+            lastDailyTs - secondsInDay,
             secondsInDay,
             false,
             undefined,
             id
           );
-          dayBeforeLastVolume =
-            Object.keys(dayBeforeLastVolumeRecord).length === 0
-              ? 0
-              : (dayBeforeLastVolumeRecord?.depositUSD + dayBeforeLastVolumeRecord?.withdrawUSD) / 2;
-          const lastWeeklyVolumeRecord = await getAggregatedDataClosestToTimestamp(
-            lastTS - secondsInWeek,
-            secondsInDay,
-            false,
-            undefined,
-            id
-          );
-          lastWeeklyVolume =
-            Object.keys(lastWeeklyVolumeRecord).length === 0
-              ? 0
-              : (lastWeeklyVolumeRecord?.depositUSD + lastWeeklyVolumeRecord?.withdrawUSD) / 2;
-          const lastMonthlyVolumeRecord = await getAggregatedDataClosestToTimestamp(
-            lastTS - secondsInDay * 30,
-            secondsInDay,
-            false,
-            undefined,
-            id
-          );
-
-          lastMonthlyVolume =
-            Object.keys(lastMonthlyVolumeRecord).length === 0
-              ? 0
-              : (lastMonthlyVolumeRecord?.depositUSD + lastMonthlyVolumeRecord?.withdrawUSD) / 2;
+          if (dayBeforeLastVolumeRecord && Object.keys(dayBeforeLastVolumeRecord).length > 0) {
+            dayBeforeLastVolume = (dayBeforeLastVolumeRecord.depositUSD + dayBeforeLastVolumeRecord?.withdrawUSD) / 2;
+          }
+          lastMonthDailyVolume.map((entry, i) => {
+            const volume = (entry.depositUSD + entry.withdrawUSD) / 2;
+            monthlyVolume += volume;
+            if (i < 7) {
+              weeklyVolume += volume;
+            }
+          });
         }
 
-        /*
-        const lastWeekHourlyData = await queryAggregatedHourlyDataTimestampRange(
-          startTimestamp,
-          currentTimestamp,
-          bridgeDbName,
-        );
-        if (!lastWeekHourlyData?.length) {
-          return null;
-        }
+        const hourlyStartTimestamp = currentTimestamp - secondsInDay;
+        const lastDayHourlyVolume = await getHourlyBridgeVolume(hourlyStartTimestamp, currentTimestamp, undefined, id);
+        if (lastDayHourlyVolume?.length) {
+          const lastHourlyVolumeRecord = lastDayHourlyVolume[lastDayHourlyVolume.length - 1];
+          lastHourlyVolume = (lastHourlyVolumeRecord.depositUSD + lastHourlyVolumeRecord.withdrawUSD) / 2;
 
-        // can use lastWeekHourlyData and lastHourlyRecord to provide more granular vol stats, but not needed at this time
-        const lastHourlyRecord = lastWeekHourlyData[lastWeekHourlyData.length - 1];
-
-        const lastTS = Math.floor(lastHourlyRecord.ts.getTime() / 1000)
-        if (typeof lastTS !== "number") {
-          return null;
+          lastDayHourlyVolume.map((entry) => {
+            const volume = (entry.depositUSD + entry.withdrawUSD) / 2;
+            const { date, depositTxs, withdrawTxs } = entry;
+            // lastDailyTs is timestamp at 00:00 UTC of *previous* day
+            if (parseInt(date) > lastDailyTs + secondsInDay) {
+              currentDayVolume += volume;
+            }
+          });
         }
-        */
 
         // can include transaction count if needed
         const dataToReturn = {
           id: id,
           name: bridgeDbName,
           displayName: displayName,
-          url: url,
+          // url: url,
           icon: iconLink,
-          volumePrevDay: lastDailyVolume ?? 0,
-          volumePrev2Day: dayBeforeLastVolume ?? 0,
-          volumePrevWeek: lastWeeklyVolume ?? 0,
-          volumePrevMonth: lastMonthlyVolume ?? 0,
+          lastHourlyVolume: lastHourlyVolume ?? 0,
+          currentDayVolume: currentDayVolume ?? 0,
+          lastDailyVolume: lastDailyVolume ?? 0,
+          dayBeforeLastVolume: dayBeforeLastVolume ?? 0,
+          weeklyVolume: weeklyVolume ?? 0,
+          monthlyVolume: monthlyVolume ?? 0,
           chains: chains,
           destinationChain: destinationChain ?? "false",
         } as any;
@@ -97,7 +82,7 @@ const getBridges = async () => {
     )
   )
     .filter((bridgeNetwork) => bridgeNetwork !== null)
-    .sort((a, b) => b.volumePrevDay - a.volumePrevDay);
+    .sort((a, b) => b.lastDailyVolume - a.lastDailyVolume);
 
   return response;
 };
