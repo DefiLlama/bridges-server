7697e3da82f04b8023e1e80cf727be17496d5f03
Cocoahomology <83151536+Cocoahomology@users.noreply.github.com>
2022-12-02 04:15:25 -0500

    add current day to getBridgeVolume

--- src/handlers/getBridgeVolume.ts
+++ src/handlers/getBridgeVolume.ts
@@ -1,7 +1,8 @@
 import { IResponse, successResponse, errorResponse } from "../utils/lambda-response";
 import wrap from "../utils/wrap";
-import { getDailyBridgeVolume } from "../utils/bridgeVolume";
+import { getDailyBridgeVolume, getHourlyBridgeVolume } from "../utils/bridgeVolume";
 import { importBridgeNetwork } from "../data/importBridgeNetwork";
+import { secondsInDay, getCurrentUnixTimestamp } from "../utils/date";
 
 const getBridgeVolume = async (chain?: string, bridgeNetworkId?: string) => {
   const queryChain = chain === "all" ? undefined : chain;
@@ -20,7 +21,41 @@ const getBridgeVolume = async (chain?: string, bridgeNetworkId?: string) => {
       });
     }
   }
-  const response = await getDailyBridgeVolume(undefined, undefined, queryChain, queryId);
+  const dailyVolumes = await getDailyBridgeVolume(undefined, undefined, queryChain, queryId);
+
+  let currentDayEntry = null as unknown;
+  const lastDailyTs = parseInt(dailyVolumes?.[dailyVolumes.length - 1]?.date) || 9999999999;
+  const currentTimestamp = getCurrentUnixTimestamp();
+  const hourlyStartTimestamp = currentTimestamp - secondsInDay;
+  const lastDayHourlyVolume = await getHourlyBridgeVolume(hourlyStartTimestamp, currentTimestamp, queryChain, queryId);
+  if (lastDayHourlyVolume?.length) {
+    let currentDayDepositUSD = 0;
+    let currentDayWithdrawUSD = 0;
+    let currentDayDepositTxs = 0;
+    let currentDayWithdrawTxs = 0;
+    lastDayHourlyVolume.map((entry) => {
+      const { date, depositTxs, withdrawTxs, depositUSD, withdrawUSD } = entry;
+      // lastDailyTs is timestamp at 00:00 UTC of *previous* day
+      if (parseInt(date) > lastDailyTs + secondsInDay) {
+        currentDayDepositUSD += depositUSD;
+        currentDayWithdrawUSD += withdrawUSD;
+        currentDayDepositTxs += depositTxs;
+        currentDayWithdrawTxs += withdrawTxs;
+      }
+    });
+    currentDayEntry = {
+      date: (lastDailyTs + secondsInDay).toString(),
+      depositUSD: currentDayDepositUSD,
+      withdrawUSD: currentDayWithdrawUSD,
+      depositTxs: currentDayDepositTxs,
+      withdrawTxs: currentDayWithdrawTxs,
+    };
+  }
+
+  let response = dailyVolumes;
+  if (currentDayEntry) {
+    response.push(currentDayEntry);
+  }
 
   return response;
 };
--- src/utils/db.js
+++ src/utils/db.js
@@ -2,10 +2,10 @@ import postgres from "postgres";
 
 const sql = postgres(
   {
-    host: process.env.PSQL_URL,
+    host: 'bridges.cz3l9ki794cf.eu-central-1.rds.amazonaws.com',
     database: "bridges",
-    username: process.env.PSQL_USERNAME,
-    password: process.env.PSQL_PW,
+    username: 'postgresmaster',
+    password: '2zbDKWZt5X6iJaGD7Oew',
     idle_timeout: 20,
     max_lifetime: 60 * 30,
   }
