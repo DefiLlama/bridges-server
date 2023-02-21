42c20039477f854d85e2470b319e4bd932aea0c5
0xngmi <0xngmi@protonmail.com>
2023-02-21 14:16:13 +0000

    avoid inserting null

--- src/handlers/runAdapter.ts
+++ src/handlers/runAdapter.ts
@@ -3,7 +3,7 @@ import bridgeNetworks from "../data/bridgeNetworkData";
 import { runAdapterToCurrentBlock } from "../utils/adapter";
 
 const handler = async (event: any) => {
-    await runAdapterToCurrentBlock(bridgeNetworks[event.bridgeIndex], true, "upsert")
+    await runAdapterToCurrentBlock(bridgeNetworks[event.bridgeIndex], false, "upsert")
 };
 
 export default wrapScheduledLambda(handler);
