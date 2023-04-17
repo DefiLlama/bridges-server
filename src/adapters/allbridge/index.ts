7eefd1ad3feb2276562806d8da557ce834d4e1cf
nemusona <nemusonaneko@protonmail.com>
2023-04-17 16:06:55 -0500

    fix adapter to detect eth withdrawals

--- src/adapters/allbridge/index.ts
+++ src/adapters/allbridge/index.ts
@@ -17,11 +17,30 @@ const constructParams = (chain: string) => {
   const depositParams = constructTransferParams(bridgeAddress, true, {
     excludeFrom: [bridgeAddress],
   });
-  const withdrawParams = constructTransferParams(bridgeAddress, false, {
-    excludeTo: [bridgeAddress],
-  });
+  // const withdrawParams = constructTransferParams(bridgeAddress, false, {
+  //   excludeTo: [bridgeAddress],
+  // });
+  const withdrawParams = {
+    target: bridgeAddress,
+    topic: "Received(address,address,uint256,uint128,bytes4)",
+    abi: [
+      "event Received(address indexed recipient, address token, uint256 amount, uint128 indexed lockId, bytes4 source)",
+    ],
+    logKeys: {
+      blockNumber: "blockNumber",
+      txHash: "transactionHash",
+    },
+    argKeys: {
+      amount: "amount",
+      token: "token",
+      to: "recipient",
+    },
+    fixedEventData: {
+      from: bridgeAddress,
+    },
+    isDeposit: false,
+  };
   eventParams.push(depositParams, withdrawParams);
-
   return async (fromBlock: number, toBlock: number) =>
     getTxDataFromEVMEventLogs("allbridge", chain as Chain, fromBlock, toBlock, eventParams);
 };
