3bd488a8bcc702dd7b60d837ff9ea9a20f5b8016
vrotend <vrotend@proton.me>
2023-06-23 20:36:29 +0300

    Update db.js

--- src/utils/db.js
+++ src/utils/db.js
@@ -1,16 +1,9 @@
 import postgres from "postgres";
 
+
 const sql = postgres(
-  {
-    host: process.env.PSQL_URL,
-    database: "cle37p03g00dhd6lff4ch90qw",
-    username: process.env.PSQL_USERNAME,
-    password: process.env.PSQL_PW,
-    port: 9004,
-    idle_timeout: 20,
-    max_lifetime: 60 * 3,
-    max: 10,
-  }
+  `postgresql://${process.env.PSQL_USERNAME}:${process.env.PSQL_PW}@${process.env.PSQL_URL}:9004/cle37p03g00dhd6lff4ch90qw`,
+  { idle_timeout: 20, max_lifetime: 60 * 3, max: 10 }
 );
 
 export { sql };
