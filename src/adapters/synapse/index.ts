98e730cc5647588516ca9a75f21894ee25fb9b61
defi-moses <jakedinero@protonmail.com>
2023-07-06 16:44:32 +0100

    addded all chains and cleaned up comments

--- src/adapters/synapse/index.ts
+++ src/adapters/synapse/index.ts
@@ -5,16 +5,62 @@ import { getTxDataFromEVMEventLogs } from "../../helpers/processTransactions";
 
 const nullAddress = "0x0000000000000000000000000000000000000000";
 
-//TODO fill in the rest of the synapse bridge contracts
 //Synapse Bridge Contracts on all supported chains
 const contractAddresses = {
-    polygon: {
-        synapseBridge: "0x8F5BBB2BB8c2Ee94639E55d5F41de9b4839C1280",
-    },
-    arbitrum: {
-        synapseBridge: "0x6F4e8eBa4D337f874Ab57478AcC2Cb5BACdc19c9",
-    },
-
+  arbitrum: {
+      synapseBridge: "0x6F4e8eBa4D337f874Ab57478AcC2Cb5BACdc19c9",
+  },
+  aurora: {
+      synapseBridge: "0xaeD5b25BE1c3163c907a471082640450F928DDFE",
+  },
+  avalanche: {
+      synapseBridge: "0xC05e61d0E7a63D27546389B7aD62FdFf5A91aACE",
+  },
+  boba: {
+      synapseBridge: "0x432036208d2717394d2614d6697c46DF3Ed69540",
+  },
+  bsc: {
+      synapseBridge: "0xd123f70AE324d34A9E76b67a27bf77593bA8749f",
+  },
+  canto: {
+      synapseBridge: "0xDde5BEC4815E1CeCf336fb973Ca578e8D83606E0",
+  },
+  cronos: {
+      synapseBridge: "0xE27BFf97CE92C3e1Ff7AA9f86781FDd6D48F5eE9",
+  },
+  dfk: {
+      synapseBridge: "0xE05c976d3f045D0E6E7A6f61083d98A15603cF6A",
+  },
+  dogechain: {
+      synapseBridge: "0x9508BF380c1e6f751D97604732eF1Bae6673f299",
+  },
+  ethereum: {
+      synapseBridge: "0x2796317b0fF8538F253012862c06787Adfb8cEb6",
+  },
+  fantom: {
+      synapseBridge: "0xAf41a65F786339e7911F4acDAD6BD49426F2Dc6b",
+  },
+  harmony: {
+      synapseBridge: "0xAf41a65F786339e7911F4acDAD6BD49426F2Dc6b",
+  },
+  moonriver: {
+      synapseBridge: "0xaeD5b25BE1c3163c907a471082640450F928DDFE",
+  },
+  moonbeam: {
+      synapseBridge: "0x84A420459cd31C3c34583F67E0f0fB191067D32f",
+  },
+  optimism: {
+      synapseBridge: "0xAf41a65F786339e7911F4acDAD6BD49426F2Dc6b",
+  },
+  polygon: {
+      synapseBridge: "0x8F5BBB2BB8c2Ee94639E55d5F41de9b4839C1280",
+  },
+  metis: {
+      synapseBridge: "0x06Fea8513FF03a0d3f61324da709D4cf06F42A5c",
+  },
+  klaytn: {
+      synapseBridge: "0xAf41a65F786339e7911F4acDAD6BD49426F2Dc6b",
+  }
 } as {
     [chain: string]: {
         synapseBridge: string;
@@ -50,7 +96,6 @@ const contractAddresses = {
 
   const TokenDepositAndSwapDepositParams: PartialContractEventParams = {
     target: "",
-    //Not sure if "contract IERC20" is the right thing here?? Need to double check
     topic:"TokenDepositAndSwap(address,uint256,contract,uint256,uint8,uint8,uint256,uint256)",
     abi: [
       "event TokenDepositAndSwap(address indexed to, uint256 chainId, address, uint256 amount, uint8 tokenIndexFrom, uint8 tokenIndexTo, uint256 minDy, uint256 deadline)",
@@ -72,7 +117,6 @@ const contractAddresses = {
 
   const TokenRedeemDepositParams: PartialContractEventParams = {
     target: "",
-    //Not sure if contract is the right thing here?? Need to double check
     topic:"TokenRedeem(address,uint256,address,uint256)",
     abi: [
       "event TokenRedeem(address indexed to, uint256 chainId, address, uint256 amount)",
@@ -94,7 +138,6 @@ const contractAddresses = {
 
   const TokenRedeemAndRemoveDepositParams: PartialContractEventParams = {
     target: "",
-    //Not sure if contract is the right thing here?? Need to double check
     topic:"TokenRedeemAndRemove(address,uint256,address,uint256,uint8,uint256,uint256)",
     abi: [
       "event TokenRedeemAndRemove(address indexed to, uint256 chainId, address, uint256 amount, uint8 swapTokenIndex, uint256 swapMinAmount, uint256 swapDeadline)",
@@ -116,7 +159,6 @@ const contractAddresses = {
 
   const TokenRedeemAndSwapDepositParams: PartialContractEventParams = {
     target: "",
-    //Not sure if contract is the right thing here?? Need to double check
     topic:"TokenRedeemAndSwap(address,uint256,address,uint256,uint8,uint8,uint256,uint256)",
     abi: [
       "event TokenRedeemAndSwap(address indexed to, uint256 chainId, address, uint256 amount, uint8 tokenIndexFrom, uint8 tokenIndexTo, uint256 minDy, uint256 deadline)",
@@ -138,7 +180,6 @@ const contractAddresses = {
 
   const TokenRedeemV2DepositParams: PartialContractEventParams = {
     target: "",
-    //Not sure if contract is the right thing here?? Need to double check
     topic:"TokenRedeemV2(bytes32,uint256,address,uint256)",
     abi: [
       "event TokenRedeemV2(bytes32 indexed to, uint256 chainId, address, uint256 amount)",
@@ -161,7 +202,6 @@ const contractAddresses = {
 //Begin outlining the withdraws
 const TokenWithdrawWithdrawParams: PartialContractEventParams = {
     target: "",
-    //Not sure if contract is the right thing here?? Need to double check
     topic:"TokenWithdraw(address,address,uint256,uint256,bytes32)",
     abi: [
       "event TokenWithdraw(address indexed to, address, uint256 amount, uint256 fee, bytes32 indexed kappa)",
@@ -183,7 +223,6 @@ const TokenWithdrawWithdrawParams: PartialContractEventParams = {
 
   const TokenWithdrawAndRemoveWithdrawParams: PartialContractEventParams = {
     target: "",
-    //Not sure if contract is the right thing here?? Need to double check
     topic:"TokenWithdrawAndRemove(address,address,uint256,uint256,uint8,uint256,uint256,bool,bytes32)",
     abi: [
       "event TokenWithdrawAndRemove(address indexed to, address, uint256 amount, uint256 fee, uint8 swapTokenIndex, uint256 swapMinAmount, uint256 swapDeadline, bool swapSuccess, bytes32 indexed kappa)",
@@ -205,7 +244,6 @@ const TokenWithdrawWithdrawParams: PartialContractEventParams = {
 
   const TokenMintWithdrawParams: PartialContractEventParams = {
     target: "",
-    //Not sure if contract is the right thing here?? Need to double check
     topic:"TokenMint(address,address,uint256,uint256,bytes32)",
     abi: [
       "event TokenMint(address indexed to, address, uint256 amount, uint256 fee, bytes32 indexed kappa)",
@@ -227,7 +265,6 @@ const TokenWithdrawWithdrawParams: PartialContractEventParams = {
 
   const TokenMintAndSwapWithdrawParams: PartialContractEventParams = {
     target: "",
-    //Not sure if contract is the right thing here?? Need to double check
     topic:"TokenMintAndSwap(address,address,uint256,uint256,uint8,uint8,uint256,uint256,bool,bytes32)",
     abi: [
       "event TokenMintAndSwap(address indexed to, address, uint256 amount, uint256 fee, uint8 tokenIndexFrom, uint8 tokenIndexTo, uint256 minDy, uint256 deadline, bool swapSuccess, bytes32 indexed kappa)",
@@ -318,10 +355,26 @@ const constructParams = (chain:string) => {
 
 
 const adapter: BridgeAdapter = {
-    //TODO add more chains
-    polygon: constructParams("polygon"),
-    arbitrum: constructParams("arbitrum"),
-  };
+  arbitrum: constructParams("arbitrum"),
+  aurora: constructParams("aurora"),
+  avalanche: constructParams("avalanche"),
+  boba: constructParams("boba"),
+  bsc: constructParams("bsc"),
+  canto: constructParams("canto"),
+  cronos: constructParams("cronos"),
+  dfk: constructParams("dfk"),
+  dogechain: constructParams("dogechain"),
+  ethereum: constructParams("ethereum"),
+  fantom: constructParams("fantom"),
+  harmony: constructParams("harmony"),
+  moonriver: constructParams("moonriver"),
+  moonbeam: constructParams("moonbeam"),
+  optimism: constructParams("optimism"),
+  polygon: constructParams("polygon"),
+  metis: constructParams("metis"),
+  klaytn: constructParams("klaytn")
+};
+
 
   export default adapter;
 
