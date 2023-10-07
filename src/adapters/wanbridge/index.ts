4af63c4783d0463ec6590e85cd6eddb88e9b9de0
lolieatapple <lolieatapple@gmail.com>
2023-10-07 15:36:00 +0800

    update for wanbridge

--- src/adapters/wanbridge/index.ts
+++ src/adapters/wanbridge/index.ts
@@ -1,122 +1,80 @@
-import {
-  BridgeAdapter,
-  PartialContractEventParams,
-} from "../../helpers/bridgeAdapter.type";
-import {getTxDataFromEVMEventLogs} from "../../helpers/processTransactions";
-import {constructTransferParams} from "../../helpers/eventParams";
+import { Chain } from "@defillama/sdk/build/general";
+import { BridgeAdapter, PartialContractEventParams } from "../../helpers/bridgeAdapter.type";
+import { constructTransferParams } from "../../helpers/eventParams";
+import { getTxDataFromEVMEventLogs } from "../../helpers/processTransactions";
 
-const contracts = {
-  arbitrum: {
-    pools: [
-      '0xf7ba155556e2cd4dfe3fe26e506a14d2f4b97613'
-    ]
-  },
-  astar: {
-    pools: [
-      '0x592de30bebff484b5a43a6e8e3ec1a814902e0b6'
-    ]
-  },
-  avax: {
-    pools: [
-      '0x74e121a34a66d54c33f3291f2cdf26b1cd037c3a'
-    ]
-  },
-  bsc: {
-    pools: [
-      '0xc3711bdbe7e3063bf6c22e7fed42f782ac82baee'
-    ]
-  },
-  clv: {
-    pools: [
-      '0xf7ba155556e2cd4dfe3fe26e506a14d2f4b97613'
-    ]
-  },
-  ethereum: {
-    pools: [
-      '0xfceaaaeb8d564a9d0e71ef36f027b9d162bc334e'
-    ]
-  },
-  fantom: {
-    pools: [
-      '0xccffe9d337f3c1b16bd271d109e691246fd69ee3'
-    ]
-  },
-  moonbeam: {
-    pools: [
-      '0x6372aec6263aa93eacedc994d38aa9117b6b95b5'
-    ]
-  },
-  moonriver: {
-    pools: [
-      '0xde1ae3c465354f01189150f3836c7c15a1d6671d'
-    ]
-  },
-  okexchain: {
-    pools: [
-      '0xf7ba155556e2cd4dfe3fe26e506a14d2f4b97613'
-    ]
-  },
-  optimism: {
-    pools: [
-      '0xc6ae1db6c66d909f7bfeeeb24f9adb8620bf9dbf' 
-    ]
-  },
-  polygon: {
-    pools: [
-      '0x2216072a246a84f7b9ce0f1415dd239c9bf201ab' 
-    ]
-  },
-  telos: {
-    pools: [
-      '0x201e5de97dfc46aace142b2009332c524c9d8d82'
-    ]
-  },
-  wan: {
-    pools: [
-      '0xe85b0d89cbc670733d6a40a9450d8788be13da47' 
-    ]
-  },
-  xdc: {
-    pools: [
-      '0xf7ba155556e2cd4dfe3fe26e506a14d2f4b97613' 
-    ]
-  }
+const gatewayAddresses = {
+  arbitrum: "0xf7ba155556e2cd4dfe3fe26e506a14d2f4b97613",
+  wanchain: "0xe85b0d89cbc670733d6a40a9450d8788be13da47",
+  ethereum: "0xfceaaaeb8d564a9d0e71ef36f027b9d162bc334e",
+  avalanche: "0x74e121a34a66d54c33f3291f2cdf26b1cd037c3a",
+  moonbeam: "0x6372aec6263aa93eacedc994d38aa9117b6b95b5",
+  moonriver: "0xde1ae3c465354f01189150f3836c7c15a1d6671d",
+  functionX: "0xdf935552fac687123c642f589296762b632a9aaf",
+  telos: "0x201e5de97dfc46aace142b2009332c524c9d8d82",
+  polygon: "0x2216072a246a84f7b9ce0f1415dd239c9bf201ab",
+  okexchain: "0xf7ba155556e2cd4dfe3fe26e506a14d2f4b97613",
+  optimism: "0xc6ae1db6c66d909f7bfeeeb24f9adb8620bf9dbf",
+  xdc: "0xf7ba155556e2cd4dfe3fe26e506a14d2f4b97613",
+  bsc: "0xc3711bdbe7e3063bf6c22e7fed42f782ac82baee",
+  astar: "0x592de30bebff484b5a43a6e8e3ec1a814902e0b6",
+  metis: "0xc6ae1db6c66d909f7bfeeeb24f9adb8620bf9dbf",
+  horizen: "0x97e0883493e8bb7a119a1e36e53ee9e7a2d3ca7b",
+  fantom: "0xccffe9d337f3c1b16bd271d109e691246fd69ee3",
+  clover: "0xf7ba155556e2cd4dfe3fe26e506a14d2f4b97613",
+  gather: "0xc6ae1db6c66d909f7bfeeeb24f9adb8620bf9dbf",
+  tron: "0xfe464ebd5bb5d95731f90aa7b9e39df920a61c97",
+  vinuchain: "0x72ccf64ee5e2c7629ee4eee3e6ad6990289178ae",
+} as {
+  [chain: string]: string;
 };
 
-const constructParams = (chain: keyof typeof contracts) => {
-  const eventParams: PartialContractEventParams[] = [];
+const nullAddress = "0x0000000000000000000000000000000000000000";
 
-  for (const poolAddress of contracts[chain].pools) {
-    eventParams.push(constructTransferParams(
-      poolAddress,
-      true
-    ),constructTransferParams(
-      poolAddress,
-      false
-    ));
+const constructParams = (chain: string) => {
+  let eventParams = [] as PartialContractEventParams[];
+  const addy = gatewayAddresses[chain];
 
-  }
+  const userLock = constructTransferParams(addy, true, {
+    excludeFrom: [addy, nullAddress],
+    excludeTo: [nullAddress],
+    includeTo: [addy],
+  });
+
+  const smgRelease = constructTransferParams(addy, false, {
+    excludeFrom: [nullAddress],
+    excludeTo: [nullAddress, addy],
+    includeFrom: [addy],
+  });
+
+  eventParams.push(userLock, smgRelease);
 
   return async (fromBlock: number, toBlock: number) =>
-    getTxDataFromEVMEventLogs("allbridge", chain, fromBlock, toBlock, eventParams);
+    await getTxDataFromEVMEventLogs("wanbridge", chain as Chain, fromBlock, toBlock, eventParams);
+};
 
-}
 const adapter: BridgeAdapter = {
-  arbitrum: constructParams('arbitrum'),
-  astar: constructParams('astar'),
-  avax: constructParams('avax'),
-  bsc: constructParams('bsc'),
-  clv: constructParams('clv'),
-  ethereum: constructParams('ethereum'),
-  fantom: constructParams('fantom'),
-  moonbeam: constructParams('moonbeam'),
-  moonriver: constructParams('moonriver'),
-  okexchain: constructParams('okexchain'),
-  optimism: constructParams('optimism'),
-  polygon: constructParams('polygon'),
-  telos: constructParams('telos'),
-  wan: constructParams('wan'),
-  xdc: constructParams('xdc'),
+  polygon: constructParams("polygon"),
+  arbitrum: constructParams("arbitrum"),
+  bsc: constructParams("bsc"),
+  ethereum: constructParams("ethereum"),
+  avalanche: constructParams("avalanche"),
+  moonbeam: constructParams("moonbeam"),
+  moonriver: constructParams("moonriver"),
+  functionX: constructParams("functionX"),
+  telos: constructParams("telos"),
+  okexchain: constructParams("okexchain"),
+  optimism: constructParams("optimism"),
+  xdc: constructParams("xdc"),
+  astar: constructParams("astar"),
+  metis: constructParams("metis"),
+  horizen: constructParams("horizen"),
+  fantom: constructParams("fantom"),
+  clover: constructParams("clover"),
+  gather: constructParams("gather"),
+  tron: constructParams("tron"),
+  vinuchain: constructParams("vinuchain"),
+  wanchain: constructParams("wanchain"),
 };
 
 export default adapter;
