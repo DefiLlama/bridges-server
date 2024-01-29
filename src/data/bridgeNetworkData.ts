a05c331a2a4aad1d4102fd9cf77fdf77bf20bf55
vrtnd <vrotend@proton.me>
2024-01-30 03:07:43 +0700

    Disable Butter network

--- src/data/bridgeNetworkData.ts
+++ src/data/bridgeNetworkData.ts
@@ -738,17 +738,17 @@ export default [
       avalanche: "avax",
     },
   },
-  {
-    id: 47,
-    displayName: "Butter Network",
-    bridgeDbName: "butternetwork",
-    iconLink: "icons:butter-network",
-    largeTxThreshold: 10000,
-    url: "",
-    chains: ["Ethereum", "Polygon", "BSC", "MAP Relay Chain"],
-    chainMapping: {
-      "map relay chain": "map",
-    },
-    destinationChain: "MAP Relay Chain",
-  },
+  // {
+  //   id: 47,
+  //   displayName: "Butter Network",
+  //   bridgeDbName: "butternetwork",
+  //   iconLink: "icons:butter-network",
+  //   largeTxThreshold: 10000,
+  //   url: "",
+  //   chains: ["Ethereum", "Polygon", "BSC", "MAP Relay Chain"],
+  //   chainMapping: {
+  //     "map relay chain": "map",
+  //   },
+  //   destinationChain: "MAP Relay Chain",
+  // },
 ] as BridgeNetwork[];
