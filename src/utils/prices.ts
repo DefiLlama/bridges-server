2b7a232773951297f7e825d37506da9888df8155
vrtnd <vrotend@proton.me>
2024-05-10 17:13:13 +0300

    fix gnosis pricing

--- src/utils/prices.ts
+++ src/utils/prices.ts
@@ -21,35 +21,34 @@ export const getSingleLlamaPrice = async (
   return null;
 };
 
+const NAME_MAPPING: Record<string, string> = {
+  "b2-mainnet": "bsquared",
+  xdai: "gnosis",
+};
+
 export const getLlamaPrices = async (tokens: string[], timestamp?: number) => {
-  let finalPrices = {} as any;
-  let remainingTokens = tokens?.map((token) =>
-    token?.includes("b2-mainnet") ? `bsquared:${token?.split(":")[1]}` : token
-  );
+  const finalPrices: { [key: string]: any } = {};
+  let remainingTokens = tokens.map((token) => {
+    const [prefix, id] = token.split(":");
+    const mappedPrefix = NAME_MAPPING[prefix] || prefix;
+    return `${mappedPrefix}:${id}`;
+  });
+
   while (remainingTokens.length > 0) {
     const url = timestamp
-      ? PRICES_API +
-        `/historical/${timestamp}/${remainingTokens
-          .slice(0, maxNumberOfPrices)
-          .reduce((acc, curr) => {
-            return acc + curr + ",";
-          }, "")
-          .slice(0, -1)}`
-      : PRICES_API +
-        `/current/${remainingTokens
-          .slice(0, maxNumberOfPrices)
-          .reduce((acc, curr) => {
-            return acc + curr + ",";
-          }, "")
-          .slice(0, -1)}`;
-    const res = await retry(async (_bail: any) => await axios.get(url));
+      ? `${PRICES_API}/historical/${timestamp}/${remainingTokens.slice(0, maxNumberOfPrices).join(",")}`
+      : `${PRICES_API}/current/${remainingTokens.slice(0, maxNumberOfPrices).join(",")}`;
+    const res = await retry(async () => await axios.get(url));
     const prices = res?.data?.coins;
-    finalPrices = { ...finalPrices, ...prices };
+    Object.assign(finalPrices, prices);
     remainingTokens = remainingTokens.slice(maxNumberOfPrices);
   }
+
   return Object.fromEntries(
-    Object.entries(finalPrices || {}).map(([key, value]) =>
-      key?.includes("bsquared") ? [`b2-mainnet:${key?.split(":")[1]}`, value] : [key, value]
-    )
+    Object.entries(finalPrices).map(([key, value]) => {
+      const [prefix, id] = key.split(":");
+      const originalPrefix = Object.entries(NAME_MAPPING).find(([, mappedName]) => mappedName === prefix)?.[0];
+      return [`${originalPrefix || prefix}:${id}`, value];
+    })
   );
 };
