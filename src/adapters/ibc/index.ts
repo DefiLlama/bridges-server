fa5006298c392423cf00424e96616a6135d1197e
0xMOZ <168462805+0xMOZ@users.noreply.github.com>
2024-06-19 15:39:16 +0300

    start block from last block in defillama
    replace exception with undefined
    
    replace nextBlock with block+1
    
    replace node fetch with axios

--- src/adapters/ibc/index.ts
+++ src/adapters/ibc/index.ts
@@ -1,13 +1,54 @@
+import retry from "async-retry"
+import axios from "axios";
+
 import { BridgeNetwork } from "../../data/types";
 import { BridgeAdapter } from "../../helpers/bridgeAdapter.type";
 import { 
   getBlockFromTimestamp, 
   getIbcVolumeByZoneId, 
-  getLatestBlockForZone
+  getLatestBlockForZone,
 } from "../../helpers/mapofzones";
 import bridges from "../../data/bridgeNetworkData";
 
 const ibcBridgeNetwork = bridges.find((bridge) => bridge.bridgeDbName === "ibc");
+const DEFILLAMA_LATEST_BLOCK_URL = "https://llama-bridges-data.s3.eu-central-1.amazonaws.com/lastRecordedBlocks.json";
+
+export const getBlockToStartFromDefillama = async (bridge: BridgeNetwork, chain: string, timestamp: number): Promise<{
+  block: number;
+} | undefined> => {
+  const response = await retry(async (_bail) => await axios.get(
+    DEFILLAMA_LATEST_BLOCK_URL
+  ))
+
+  const data = await response.data;
+
+  if (!data) {
+    console.log(`Failed to fetch latest block for ${chain}`);
+    return undefined;
+  }
+
+  const chainName = findChainName(bridge, chain);
+
+  const block = data[`ibc-${chainName}`];
+
+  if (block) {
+    return {
+      block: block.endBlock + 1,
+    };
+  }
+  // for new chains may be they have not been added to defillama yet
+  // fallback to the timestamp
+  const blockFromTimestamp = await ibcGetBlockFromTimestamp(bridge, timestamp, chain, 'First');
+  if (!blockFromTimestamp) {
+    console.log(`Could not find block for ${chain} from timestamp ${timestamp}`);
+    return undefined;
+  }
+
+  return {
+    block: blockFromTimestamp.block,
+  };
+
+}
 
 export const getLatestBlockForZoneFromMoz = async (zoneId: string): Promise<{
   number: number;
@@ -41,6 +82,18 @@ export const findChainId = (bridgeNetwork: BridgeNetwork, chain: string) => {
     return bridgeNetwork.chainMapping[chain];
   } else if (Object.values(bridgeNetwork.chainMapping).includes(chain)) {
     return chain;
+  }
+}
+
+export const findChainName = (bridgeNetwork: BridgeNetwork, chainId: string) => {
+  if (bridgeNetwork.chainMapping === undefined) {
+    throw new Error("Chain mapping is undefined for ibc bridge network.");
+  }
+
+  for (const [key, value] of Object.entries(bridgeNetwork.chainMapping)) {
+    if (value === chainId) {
+      return key;
+    }
   }
 }
 
