a6c71795669988b2dce96553fc6c2d3e264d63d2
cuegod <hengxingtianxia@gmail.com>
2024-07-05 18:57:05 +0800

    add minibridge

--- src/adapters/index.ts
+++ src/adapters/index.ts
@@ -61,6 +61,7 @@ import owlto from "./owlto";
 import zkbridge from "./zkbridge";
 import helixbridge from "./helixbridge"
 import memebridge from "./memebridge"
+import minibridge from "./minibridge"
 
 export default {
   polygon,
@@ -125,6 +126,7 @@ export default {
   zkbridge111: zkbridge,
   helixbridge,
   memebridge,
+  minibridge,
 } as {
   [bridge: string]: BridgeAdapter;
 };
