b87c01ed3a07db1414ba0bf6ff9b76d6c4d567e3
0xfisher6 <162862570+0xfisher6@users.noreply.github.com>
2024-07-11 17:36:55 +0800

    Update index.ts

--- src/adapters/index.ts
+++ src/adapters/index.ts
@@ -61,6 +61,7 @@ import owlto from "./owlto";
 import zkbridge from "./zkbridge";
 import helixbridge from "./helixbridge"
 import memebridge from "./memebridge"
+import cometbridge from "./cometbridge"
 
 export default {
   polygon,
@@ -125,6 +126,7 @@ export default {
   zkbridge111: zkbridge,
   helixbridge,
   memebridge,
+  cometbridge
 } as {
   [bridge: string]: BridgeAdapter;
 };
