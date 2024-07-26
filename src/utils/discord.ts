18175bd2313e5d3919510e0417103317918611ec
vrtnd <vrotend@proton.me>
2024-07-26 11:45:45 +0300

    discord message fix

--- src/utils/discord.ts
+++ src/utils/discord.ts
@@ -2,13 +2,19 @@ import fetch from "node-fetch";
 
 export async function sendDiscordText(message: string): Promise<void> {
   try {
-    await fetch(process.env.DISCORD_WEBHOOK as string, {
+    const res = await fetch(process.env.DISCORD_WEBHOOK as string, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
       body: JSON.stringify({ content: message }),
     });
+    if (!res.ok) {
+      console.error("Failed to send message to Discord:", res.statusText);
+    }
+    if (res.ok) {
+      console.log("Message sent to Discord:", message);
+    }
   } catch (error) {
     console.error("Failed to send message to Discord:", error);
   }
