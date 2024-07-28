ebab85319668af3ec82d765ace84f75e58705c92
vrtnd <vrotend@proton.me>
2024-07-28 17:01:36 +0300

    Revert "solana rpc"
    This reverts commit 128bbd6a918d095eeb39289e6b1cfc5a1c3573e8.

--- src/helpers/solana.ts
+++ src/helpers/solana.ts
@@ -1,8 +1,7 @@
 import { Connection } from "@solana/web3.js";
 
 export const getConnection = (): Connection => {
-  const rpc =
-    process.env["SOLANA_RPC"] ?? "https://mainnet.helius-rpc.com/?api-key=c73a1568-f94a-4f7f-a903-65e632a66266";
+  const rpc = process.env["SOLANA_RPC"] ?? "https://api.mainnet-beta.solana.com";
   const connection = new Connection(rpc);
   const getBlock = async (block: number) => {
     return new Connection(rpc).getBlock(block, {
