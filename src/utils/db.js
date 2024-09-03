801f2e3e65cc48fa594ced7cd8a840e5d7e00146
vrotend <vrotend@proton.me>
2024-09-04 00:04:20 +0300

    use pgbouncer

--- src/utils/db.js
+++ src/utils/db.js
@@ -2,7 +2,7 @@ import postgres from "postgres";
 
 const connectionString =
   process.env.DB_URL ??
-  `postgresql://${process.env.PSQL_USERNAME}:${process.env.PSQL_PW}@${process.env.PSQL_URL}:6432/cle37p03g00dhd6lff4ch90qw`;
+  `postgresql://${process.env.PSQL_USERNAME}:${process.env.PSQL_PW}@${process.env.PSQL_URL}:6433/cle37p03g00dhd6lff4ch90qw`;
 
 const sql = postgres(connectionString, { idle_timeout: 200, max_lifetime: 60 * 5, max: 10 });
 
