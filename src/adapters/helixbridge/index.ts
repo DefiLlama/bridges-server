fc151b2a14b10f86153feccb5831e8dc2118130b
fewensa <37804932+fewensa@users.noreply.github.com>
2024-09-24 06:07:16 +0000

    Update helixbridge (#1)
    * format and update link
    
    * Update parse
    
    * add more chains
    
    * update chains
    
    * end coma
    
    ---------
    
    Co-authored-by: xiaoch05 <xiaoch2010@gmail.com>

--- src/adapters/helixbridge/index.ts
+++ src/adapters/helixbridge/index.ts
@@ -2,7 +2,7 @@ import type { BridgeAdapter, PartialContractEventParams } from "../../helpers/br
 import { getTxDataFromEVMEventLogs } from "../../helpers/processTransactions";
 import type { Chain } from "@defillama/sdk/build/general";
 
-const depositParams = (contractAddress: string): PartialContractEventParams => {
+const depositParams = (chain: Chain, contractAddress: string): PartialContractEventParams => {
   return {
     target: contractAddress,
     topic: "TokenLocked((uint256,address,address,address,uint112,uint112,address,uint256),bytes32,uint112,uint112)",
@@ -15,8 +15,20 @@ const depositParams = (contractAddress: string): PartialContractEventParams => {
     },
     argKeys: {
       token: "params.sourceToken",
-      amount: "targetAmount",
+      amount: "params.amount",
       to: "params.receiver",
+    },
+    argGetters: {
+      token: (args: any) => {
+        if (chain !== 'darwinia') return undefined;
+
+        const sourceToken = args.params?.sourceToken;
+        if (sourceToken != '0x0000000000000000000000000000000000000000') {
+          return undefined;
+        }
+
+        return '0xe7578598aac020abfb918f33a20fad5b71d670b4';
+      },
     },
     txKeys: {
       from: "from",
@@ -25,43 +37,53 @@ const depositParams = (contractAddress: string): PartialContractEventParams => {
   };
 }
 
-const withdrawalParams = (contractAddress: string): PartialContractEventParams => {
+const withdrawalParams = (chain: Chain, contractAddress: string): PartialContractEventParams => {
   return {
     target: contractAddress,
-    topic: "TransferFilled(bytes32,address)",
+    topic: "TransferFilledExt(bytes32,(uint256,address,address,address,uint112,uint112,address,uint256))",
     abi: [
-      "event TransferFilled(bytes32 transferId, address provider)",
+      "event TransferFilledExt(bytes32 transferId, (uint256 remoteChainId,address provider,address sourceToken, address targetToken, uint112 sourceAmount, uint112 targetAmount, address receiver, uint256 timestamp) params)",
     ],
     logKeys: {
       blockNumber: "blockNumber",
       txHash: "transactionHash",
     },
     txKeys: {
+      from: "from",
       to: "to",
     },
-    inputDataExtraction: {
-      inputDataABI: [
-        "function relay((uint256 remoteChainId, address provider, address sourceToken, address targetToken, uint112 sourceAmount, uint112 targetAmount, address receiver, uint256 timestamp) params, bytes32 expectedTransferId, bool relayBySelf)",
-      ],
-      inputDataFnName: "relay",
-      inputDataKeys: {
-        token: "params.targetToken",
-        amount: "params.targetAmount",
-      },
+    argKeys: {
+      token: "params.targetToken",
+      amount: "params.targetAmount",
+      to: "params.receiver",
     },
-    fixedEventData: {
-      from: contractAddress,
+    argGetters: {
+      token: (args: any) => {
+        console.log(chain);
+        if (chain !== 'darwinia') return undefined;
+
+        const targetToken = args.params?.targetToken;    
+        if (targetToken != '0x0000000000000000000000000000000000000000') {
+          return undefined;
+        }
+
+        return '0xe7578598aac020abfb918f33a20fad5b71d670b4';
+      },
     },
     isDeposit: false,
   };
 }
 
 const constructParams = (chain: Chain) => {
-  const contractAddress = "0xbA5D580B18b6436411562981e02c8A9aA1776D10"
+  let contractAddress = '0xbA5D580B18b6436411562981e02c8A9aA1776D10';
+
+  if (chain === 'blast') {
+    contractAddress = '0xB180D7DcB5CC161C862aD60442FA37527546cAFC';
+  }
 
   const eventParams: PartialContractEventParams[] = [
-    depositParams(contractAddress),
-    withdrawalParams(contractAddress)
+    depositParams(chain, contractAddress),
+    withdrawalParams(chain, contractAddress),
   ];
 
   return async (fromBlock: number, toBlock: number) =>
@@ -78,6 +100,10 @@ const adapter: BridgeAdapter = {
   scroll: constructParams("scroll"),
   optimism: constructParams("optimism"),
   gnosis: constructParams("xdai"),
+  blast: constructParams("blast"),
+  moonbeam: constructParams("moonbeam"),
+  base: constructParams("base"),
+  avalanche: constructParams("avalanche"),
 };
 
 export default adapter;
