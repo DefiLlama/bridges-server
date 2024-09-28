8e3974acf8fb6f4703a7ed33917ad330f99f8349
vrtnd <vrotend@proton.me>
2024-09-28 14:36:55 +0300

    Run adapter by name

---
+++ src/handlers/runAdapterByName.ts
@@ -0,0 +1,40 @@
+import { wrapScheduledLambda } from "../utils/wrap";
+import bridgeNetworks from "../data/bridgeNetworkData";
+import { runAdapterToCurrentBlock } from "../utils/adapter";
+import { sql } from "../utils/db";
+import { BridgeNetwork } from "../data/types";
+
+const handler = async (event: { bridgeName: string }) => {
+  try {
+    const { bridgeName } = event;
+
+    if (!bridgeName) {
+      throw new Error("Bridge name is required");
+    }
+
+    const bridge = bridgeNetworks.find((b: BridgeNetwork) => b.bridgeDbName === bridgeName);
+
+    if (!bridge) {
+      throw new Error(`Bridge ${bridgeName} not found`);
+    }
+
+    const lastRecordedBlocks = await sql`SELECT jsonb_object_agg(bridge_id::text, subresult) as result
+    FROM (
+        SELECT bridge_id, jsonb_build_object('startBlock', MIN(tx_block), 'endBlock', MAX(tx_block)) as subresult
+        FROM bridges.transactions
+        GROUP BY bridge_id
+    ) subquery;
+    `;
+
+    console.log(`Running adapter for ${bridgeName}`);
+    await runAdapterToCurrentBlock(bridge, true, "ignore", lastRecordedBlocks);
+    console.log(`Adapter for ${bridgeName} ran successfully`);
+  } catch (e) {
+    console.error(`Adapter ${event.bridgeName} failed: ${JSON.stringify(e)}`);
+    throw e;
+  } finally {
+    await sql.end();
+  }
+};
+
+export default wrapScheduledLambda(handler);
