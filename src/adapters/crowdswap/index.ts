f7b47abb61504bf5499b80b274e2d4ac6a8ee6a7
Nasser Safarinia <nassersafari@gmail.com>
2024-10-16 10:22:44 +0200

    BLOC-3482: fixing active chains

--- src/adapters/crowdswap/index.ts
+++ src/adapters/crowdswap/index.ts
@@ -10,7 +10,7 @@ const gatewayAddresses = {
   optimism: "0x1B3aE33ff0241999854C05B0CdF821DE55A4404A",
   arbitrum: "0x99a68649E927774680e9D3387BF8cCbF93B45230",
   defi: "0x9092fCF5Ea1E22f2922eEa132D2931CDd795ab53",
-  rootstock: "0x9Ff74eEA1e7f0f8eE437b70d68F7Cdc1a1030642"
+  rsk: "0x9Ff74eEA1e7f0f8eE437b70d68F7Cdc1a1030642"
 } as {
   [chain: string]: string;
 };
@@ -20,11 +20,11 @@ let routerAddresses = {} as {
   [chain: string]: string[];
 };
 
-const activeChains = ["ethereum", "bsc", "polygon", "optimism", "arbitrum"];
+const activeChains = ["ethereum", "bsc", "polygon", "optimism", "arbitrum", "defi", "rsk"];
 
-for (const chain of activeChains) {
-  if (!routerAddresses[chain]) {
-    routerAddresses[chain] = ["0x549D287218E5fc9D07A91Fe2e1337D5c21B808B2", "0x30462a4863a3db9233006a87320a8e07c4a71a36"]
+for(const chain of activeChains){
+  if(!routerAddresses[chain]){
+    routerAddresses[chain] = ["0x549D287218E5fc9D07A91Fe2e1337D5c21B808B2","0x30462a4863a3db9233006a87320a8e07c4a71a36"]
   }
 }
 
@@ -34,18 +34,18 @@ const constructParams = (chain: string) => {
   const routers = routerAddresses[chain];
 
   for (const router of Object.values(routers)) {
-    const deposit: PartialContractEventParams = {
+    const deposit :PartialContractEventParams = {
       target: router,
       topic: "MessageSent(bytes32,uint256,uint256,uint256,address,address,uint64)",
       abi: ["event MessageSent(bytes32 indexed messageId, uint256 sourceAmount, uint256 destinationAmount, uint256 destinationMinAmount, address sourceTokenAddress, address destinationTokenAddress, uint64 indexed destinationChainId)"],
       isDeposit: true,
       logKeys: {
-        blockNumber: "blockNumber",
-        txHash: "transactionHash",
+        blockNumber:  "blockNumber",
+        txHash:  "transactionHash",
       },
       argKeys: {
         amount: "sourceAmount",
-        token: "sourceTokenAddress",
+        token: "sourceTokenAddress"
       },
       fixedEventData: {
         to: gatewayAddresses[chain],
@@ -53,7 +53,7 @@ const constructParams = (chain: string) => {
       },
     };
 
-    const withdraw: PartialContractEventParams = {
+    const withdraw :PartialContractEventParams = {
       target: router,
       topic: "MessageCompleted(bytes32,uint256,address,address)",
       abi: ["event MessageCompleted(bytes32 indexed messageId, uint256 destinationAmount, address destinationTokenAddress, address receiver)"],
@@ -84,8 +84,8 @@ const adapter: BridgeAdapter = {
   ethereum: constructParams("ethereum"),
   arbitrum: constructParams("arbitrum"),
   optimism: constructParams("optimism"),
-  defi: constructParams("defi"),
-  rootstock: constructParams("rootstock"),
+  // defi: constructParams("defi"),
+  // rsk: constructParams("rsk"),
 };
 
 export default adapter;
