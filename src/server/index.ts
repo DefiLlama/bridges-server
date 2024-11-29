259629f2310b96a1909e28e72ad865613d4cd18a
vrotend <vrotend@proton.me>
2024-11-29 15:51:15 +0300

    Update index.ts

--- src/server/index.ts
+++ src/server/index.ts
@@ -53,6 +53,7 @@ const start = async () => {
     server.get("/healthcheck", (_, reply) => {
       return reply.send("OK");
     });
+    
 
     const port = process.env.PORT ? parseInt(process.env.PORT) : 3000;
     await server.listen({ port, host: "0.0.0.0" });
