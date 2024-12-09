a4d61e2bfa70cbccfec1821bc2e0333fcb1218f6
vrtnd <vrotend@proton.me>
2024-12-09 21:23:48 +0300

    shuffle pool

--- src/server/jobs/runAllAdapters.ts
+++ src/server/jobs/runAllAdapters.ts
@@ -1,6 +1,7 @@
 import bridgeNetworks from "../../data/bridgeNetworkData";
 import { sql } from "../../utils/db";
 import { runAdapterToCurrentBlock } from "../../utils/adapter";
+import { PromisePool } from "@supercharge/promise-pool";
 
 export const runAllAdapters = async () => {
   const lastRecordedBlocks = await sql`SELECT jsonb_object_agg(bridge_id::text, subresult) as result
@@ -17,15 +18,16 @@ export const runAllAdapters = async () => {
     console.error("Failed to store last recorded blocks");
     console.error(e);
   }
+  const shuffledBridgeNetworks = bridgeNetworks.sort(() => Math.random() - 0.5);
 
-  await Promise.all(
-    bridgeNetworks.map(async (adapter) => {
+  await PromisePool.withConcurrency(20)
+    .for(shuffledBridgeNetworks)
+    .process(async (adapter) => {
       try {
         await runAdapterToCurrentBlock(adapter, true, "upsert", lastRecordedBlocks[0].result);
       } catch (e) {
         console.error(`Failed to run adapter ${adapter.bridgeDbName}`);
         console.error(e);
       }
-    })
-  );
+    });
 };
