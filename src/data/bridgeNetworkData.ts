7ef1f5a0de30ea58aadedaa83d19809e6d726623
EnzoPlayer0ne <enzo@desiage.org>
2025-12-05 16:08:35 +0100

    whitespace

--- src/data/bridgeNetworkData.ts
+++ src/data/bridgeNetworkData.ts
@@ -2715,6 +2715,6 @@ export default [
     largeTxThreshold: 10000,
     url: "https://1sec.to/",
     chains: ["Ethereum", "Arbitrum", "Base", "ICP"],
-     destinationChain: "ICP"
+    destinationChain: "ICP"
   },
 ] as BridgeNetwork[];
